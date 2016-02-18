6cabbbf3183b3bdfaea1fb678429e128df15444c
Shawn Lin <shawnlin305@gmail.com>
2016-02-18 17:33:10 +0800

    Refine testing code.

--- moveZeros/moveZeros.go
+++ moveZeros/moveZeros.go
@@ -1,14 +1,11 @@
 package moveZeros
 
-import "fmt"
-
 func MvZeros(nums []int) {
 	zeroIndex := 0
 	numIndex := 0
 	arryLen := len(nums)
-	fmt.Println("arryLen : ", arryLen)
 
-	for (zeroIndex < arryLen && numIndex < arryLen) {
+	for zeroIndex < arryLen && numIndex < arryLen {
 		if nums[zeroIndex] != 0 {
 			zeroIndex++
 			numIndex = zeroIndex
@@ -18,15 +15,10 @@ func MvZeros(nums []int) {
 			numIndex++
 			continue
 		}
-		nums[zeroIndex] = nums[numIndex];
-		nums[numIndex] = 0;
-		numIndex++;
-		zeroIndex++;
+		nums[zeroIndex] = nums[numIndex]
+		nums[numIndex] = 0
+		numIndex++
+		zeroIndex++
 	}
 
-	for i := 0; i < arryLen; i++ {
-		fmt.Println(nums[i])
-	}
 }
-
-
--- moveZeros/moveZeros_test.go
+++ moveZeros/moveZeros_test.go
@@ -1,13 +1,21 @@
 package moveZeros_test
 
 import (
-	"testing"
+	"fmt"
 	"leetcodeGo/moveZeros"
+	"testing"
 )
 
-
 func TestMvZeros(t *testing.T) {
-	sample := []int{0, 1, 2, 3, 4, 0, 5, 6}
-	moveZeros.MvZeros(sample)
+	_sampleArray := [][]int{
+		{0, 1, 2, 3, 4, 0, 5, 6},
+		{7, 6, 4, 0, 2, 2, 0, 1},
+	}
+
+	for i := 0; i < len(_sampleArray); i++ {
+		fmt.Println("Input array as :", _sampleArray[i])
+		moveZeros.MvZeros(_sampleArray[i])
+		fmt.Println("Output array as:", _sampleArray[i])
+		fmt.Println("--------------------------------")
+	}
 }
-	
